6af72bcf1ffaf8aa91a03ad0721f6f561c66bb39
leoarthurwall <leowall93@gmail.com>
2022-09-04 23:07:54 +0100

    added location and data state

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,8 +3,10 @@ import React, { useState } from "react";
 import axios from 'axios';
 
 function App() {
+const [data, setData] = useState({});
+const [location, setLocation] = useState('')
 
-// const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=39d58dd0e66853d63d1cdefbad6c7a37`
+const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=39d58dd0e66853d63d1cdefbad6c7a37`
 
   return (
 
