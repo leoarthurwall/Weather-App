019c2b800cc3d4ca13620a6e7efadef02a4e140d
leoarthurwall <leowall93@gmail.com>
2022-09-04 22:59:07 +0100

    added basic css and hard coded jsx. Also signed up to weather api site and obtained api key. Ran the url to get some dummy json data

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
+import React, { useState } from "react";
+import axios from 'axios';
 
 function App() {
+
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=39d58dd0e66853d63d1cdefbad6c7a37`
+
   return (
-    <div className="App">
-      <input className='InputField' type="text" placeholder="Search by city..." />
-    </div>
-  );
+
+
+ 
+      <div className="app">
+        {/* <input
+          className="InputField"
+          type="text"
+          placeholder="Search by city..."
+        /> */}
+        <div className='container'>
+          <div className='top'>
+            <div className='location'>
+              <p>London</p>
+            </div>
+            <div className='temp'>
+              <h1>20°c</h1>
+            </div>
+            <div className='description'>
+              <p>Clouds</p>
+            </div>
+          </div>
+          <div className='bottom'>
+            <div className='feels'>
+              <p className='bold'>17°c</p>
+              <p>Feels Like</p>
+            </div>
+            <div className='humidity'>
+              <p className='bold'>25%</p>
+              <p>Humidity</p>
+            </div>
+            <div className='wind'>
+              <p className='bold'>15mph</p>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+  )
 }
 
 export default App;
