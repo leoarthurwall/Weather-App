e6ee9d936c41dd2e3ffcddfb593bc52dc6f6f4a8
leoarthurwall <leowall93@gmail.com>
2022-09-05 00:15:18 +0100

    added ternaty operator to conditionally render bottom section only when name has been added in. also changedtemp units to celcius

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,68 +1,73 @@
-import './App.css';
+import "./App.css";
 import React, { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
-const [data, setData] = useState({});
-const [location, setLocation] = useState('')
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=39d58dd0e66853d63d1cdefbad6c7a37`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=39d58dd0e66853d63d1cdefbad6c7a37`;
 
-const searchLocation = (event) => {
-  if (event.key === 'Enter') {
-    axios.get(url).then((response) => {
-      setData(response.data)
-      console.log(response.data)
-    })
-  }
-  
-  }
-  
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
+
   return (
-
-
- 
-      <div className="app">
-        <div className='search'>
+    <div className="app">
+      <div className="search">
         <input
           value={location}
           type="text"
-          onChange={event => setLocation(event.target.value)}
+          onChange={(event) => setLocation(event.target.value)}
           onKeyPress={searchLocation}
           placeholder="Enter Location..."
         />
-        </div>
-       
-        <div className='container'>
-          <div className='top'>
-            <div className='location'>
-              {data.sys ? <p>{data.name}, {data.sys.country}</p> : null}
-            </div>
-            <div className='temp'>
-              {data.main ? <h1>{data.main.temp}°c</h1> : null}
-            </div>
-            <div className='description'>
-              {data.weather ? <p>{data.weather[0].description}</p> : null}
-            </div>
+      </div>
+
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            {data.sys ? (
+              <p>
+                {data.name}, {data.sys.country}
+              </p>
+            ) : null}
           </div>
-          <div className='bottom'>
-            <div className='feels'>
-              {data.main ? <p className='bold'>{data.main.feels_like}°c</p> : null}
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp}°c</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].description}</p> : null}
+          </div>
+        </div>
+
+              {/* bottom only renders when data.name is defined */}
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like}°c</p>
+              ) : null}
               <p>Feels Like</p>
             </div>
-            <div className='humidity'>
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
-            <div className='wind'>
-              {data.main? <p className='bold'>{data.wind.speed}mph</p> : null}
+            <div className="wind">
+              {data.main ? <p className="bold">{data.wind.speed}mph</p> : null}
               <p>Wind Speed</p>
             </div>
           </div>
-        </div>
+        )}
       </div>
-     
-  )
+    </div>
+  );
 }
 
 export default App;
